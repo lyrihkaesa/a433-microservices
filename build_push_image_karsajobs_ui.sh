dd4f1fd0f012a50a00af46ff98b467b3908466cf
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-03-11 00:54:03 +0700

    add: `build_push_image_karsajobs_ui.sh`

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Nama image yang akan dibuat
+IMAGE_NAME="karsajobs-ui"
+# Tag untuk image
+TAG="latest"
+
+# Mengubah nama image agar sesuai dengan format GitHub Packages (GitHub Container Registry)
+NEW_IMAGE_NAME="ghcr.io/lyrihkaesa/$IMAGE_NAME:$TAG"
+
+# Membuat Docker image dari Dockerfile dengan nama dan tag yang telah ditentukan
+docker build -t $NEW_IMAGE_NAME .
+
+# Login ke GitHub Packages (GitHub Container Registry)
+echo $PAT | docker login ghcr.io --username lyrihkaesa --password-stdin
+
+# Mengunggah image ke GitHub Packages (GitHub Container Registry)
+docker push $NEW_IMAGE_NAME
